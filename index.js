b0300a4e964bdc4fea652e29b8803342150c2b57
agus tuslam <agustuslam12@gmail.com>
2023-02-27 14:14:35 +0700

    update copy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 let password = document.getElementById("password")
 let hash = document.getElementById("hash")
+let decrypt = document.getElementById("decrypt")
 let hasilpass = document.getElementById("hasilpass")
 let copyIcon = document.querySelector(".password-box .copy-icon")
 
@@ -27,18 +28,65 @@ function getPassword(){
 
     // document.getElementById('hash').innerHTML = CryptoJS.MD5(newPassword);
 
-    if(newPassword){
-        var hashed = CryptoJS.MD5(newPassword);
-        hash.value = hashed;
-    }else{
+    var crypt = {
+        // (B1) THE SECRET KEY
+        secret : "CIPHERKEY",
+  
+        // (B2) ENCRYPT
+        encrypt : clear => {
+          var cipher = CryptoJS.AES.encrypt(clear, crypt.secret);
+          return cipher.toString();
+        },
+  
+        // (B3) DECRYPT
+        decrypt : cipher => {
+          var decipher = CryptoJS.AES.decrypt(cipher, crypt.secret);
+          return decipher.toString(CryptoJS.enc.Utf8);
+        }
+      };
+
+      if(newPassword){
+        var cipher = crypt.encrypt(newPassword)
+        hash.value = cipher
+        var decipher = crypt.decrypt(cipher);
+        decrypt.value = decipher
+
+      }else{
         alert('Password is Missing')
     }
+  
+//copy passInput's value on copyIcon click
+//copy passInput's value on copyIcon click
+copyIcon.addEventListener("click", () => {
+    navigator.clipboard.writeText(password.value);
+    copyIcon.classList.replace("uil-copy", "uil-file-check-alt"); //replace icon
+    // navigator.clipboard.writeText(hash.value);
+    // copyIcon.classList.replace("uil-copy", "uil-file-check-alt"); //replace icon
+  });
+
+}
+function reset(){
+    document.getElementById('password').value ='';
+    document.getElementById('hash').value = '';
+    document.getElementById('decrypt').value = '';
+}
+
+    // if(newPassword){
+    //     var hashed = CryptoJS.SHA256(newPassword)
+    //     hash.value = hashed;
+
+    //     // var decipher = CryptoJS.AES.decrypt(hashed)
+    //     // decrypt.value = decipher
+
+    // }else{
+    //     alert('Password is Missing')
+    // }
 
     //     setTimeout(() => {
     //     alert('password has been generated')
     // }, 200)
     
-}
+
 
 // const generatePassword = () => {
 //     let newPassword = "";
@@ -53,15 +101,3 @@ function getPassword(){
 //   };
   
 
-//copy passInput's value on copyIcon click
-//copy passInput's value on copyIcon click
-copyIcon.addEventListener("click", () => {
-    navigator.clipboard.writeText(password.value);
-    copyIcon.classList.replace("uil-copy", "uil-file-check-alt"); //replace icon
-  });
-
-
-function reset(){
-    document.getElementById('password').value ='';
-    document.getElementById('hash').value = '';
-}
